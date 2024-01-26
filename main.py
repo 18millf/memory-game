76b6aefa1dbd074cd1b8be02725eab97de0d80b8
Freddie Mills <18millf@ola.org.uk>
2024-01-26 15:41:32 +0000

    v0.1.0

---
+++ main.py
@@ -0,0 +1,12 @@
+from kivy.app import App
+
+from game_window import *
+from button_grid import *
+from revealing_button import *
+
+class MemoryGameApp(App):
+    def build(self):
+        return GameWindow()
+
+app: App = MemoryGameApp()
+app.run()
