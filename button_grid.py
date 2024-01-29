ec92ad3f9ce5dd384476854226b9970a00b8d8e9
Millzy <millzyyt08@gmail.com>
2024-01-29 14:03:14 +0000

    initialise and shuffle values

--- button_grid.py
+++ button_grid.py
@@ -1,4 +1,17 @@
 from kivy.uix.gridlayout import GridLayout
+from random import shuffle
+
+WIDTH: int = 5
+HEIGHT: int = 4
+AREA: int = WIDTH * HEIGHT
+
 
 class ButtonGrid(GridLayout):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        linear_values = [i // 2 + 1 for i in range(AREA)]
+        shuffle(linear_values)
+        self.values = [linear_values[i:i + WIDTH] for i in range(0, AREA, WIDTH)]
+
+        print(self.values)
