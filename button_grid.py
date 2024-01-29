e2957ce83ca95ab44ca3ea27d919364ef69b8d30
Millzy <millzyyt08@gmail.com>
2024-01-29 14:54:30 +0000

    on_click logic

--- button_grid.py
+++ button_grid.py
@@ -12,6 +12,11 @@ class ButtonGrid(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+        self.revealed: [[bool]] = [[False for _ in range(WIDTH)] for __ in range(HEIGHT)]
+        self.first_clicked: (int, int) = None
+        self.second_clicked: (int, int) = None
+        self.buttons: list[list[RevealingButton]] = [[RevealingButton(0, 0, 0) for _ in range(WIDTH)] for __ in range(HEIGHT)]
+
         linear_values = [i // 2 + 1 for i in range(AREA)]
         shuffle(linear_values)
         self.values = [linear_values[i:i + WIDTH] for i in range(0, AREA, WIDTH)]
@@ -19,4 +24,7 @@ class ButtonGrid(GridLayout):
         for x in range(WIDTH):
             for y in range(HEIGHT):
                 button: RevealingButton = RevealingButton(x, y, self.values[y][x])
+                self.buttons[y][x] = button
                 self.add_widget(button)
+
+        print(self.buttons)
--- revealing_button.py
+++ revealing_button.py
@@ -6,12 +6,14 @@ class RevealingButton(Button):
     def __init__(self, row: int, col: int, value: int, **kwargs):
         super().__init__(**kwargs)
 
+        # this needs to be imported in the functions where its used otherwise a circular dependency is created
+        from button_grid import ButtonGrid
+
+        self.parent: ButtonGrid
+
         self.row: int = row
         self.col: int = col
         self.value: str = str(value)
-
-        self.show()
-        Clock.schedule_once(lambda dt: self.hide(), 10)
 
     def show(self):
         self.text = self.value
@@ -20,3 +22,52 @@ class RevealingButton(Button):
     def hide(self):
         self.text = ""
         self.disabled = False
+
+    def hide_schedule_callback(self, other):
+        from button_grid import ButtonGrid
+        self.parent: ButtonGrid
+
+        self.hide()
+        other.hide()
+
+        self.parent.first_clicked = None
+        self.parent.second_clicked = None
+
+    def on_press(self, *args):
+        from button_grid import ButtonGrid
+        self.parent: ButtonGrid
+
+        if (self.parent.revealed[self.col][self.row] or
+                self.parent.first_clicked == (self.col, self.row) or
+                self.parent.second_clicked is not None):
+            return
+
+        if self.parent.first_clicked is None:
+            self.parent.first_clicked = (self.col, self.row)
+            self.show()
+
+        else:
+            other_button_coords: (int, int) = self.parent.first_clicked
+            other_button: RevealingButton = self.parent.buttons[other_button_coords[0]][other_button_coords[1]]
+
+            if self.value == other_button.value:
+                self.parent.revealed[self.col][self.row] = True
+                self.parent.revealed[other_button.col][other_button.row] = True
+
+                self.parent.first_clicked = None
+                self.parent.second_clicked = None
+
+                all_revealed = True
+                for row in self.parent.revealed:
+                    for val in row:
+                        if not val:
+                            all_revealed = False
+                            break
+                    if not all_revealed:
+                        break
+
+                if all_revealed:
+                    self.parent.parent.ids["status_test"].text = "You win!"
+
+            else:
+                Clock.schedule_once(lambda _: self.hide_schedule_callback(other_button), 2)
