1645569cc0e1c2c97614af6e4734428ee7a43499
Millzy <millzyyt08@gmail.com>
2024-01-29 14:10:38 +0000

    v0.3

--- button_grid.py
+++ button_grid.py
@@ -1,6 +1,8 @@
 from kivy.uix.gridlayout import GridLayout
 from random import shuffle
 
+from revealing_button import RevealingButton
+
 WIDTH: int = 5
 HEIGHT: int = 4
 AREA: int = WIDTH * HEIGHT
@@ -14,4 +16,7 @@ class ButtonGrid(GridLayout):
         shuffle(linear_values)
         self.values = [linear_values[i:i + WIDTH] for i in range(0, AREA, WIDTH)]
 
-        print(self.values)
+        for x in range(WIDTH):
+            for y in range(HEIGHT):
+                button: RevealingButton = RevealingButton(x, y, self.values[y][x])
+                self.add_widget(button)
--- revealing_button.py
+++ revealing_button.py
@@ -2,4 +2,11 @@ from kivy.uix.button import Button
 
 
 class RevealingButton(Button):
-    pass
+    def __init__(self, row: int, col: int, value: int, **kwargs):
+        super().__init__(**kwargs)
+
+        self.row: int = row
+        self.col: int = col
+        self.value: int = value
+
+        self.text = str(value)
